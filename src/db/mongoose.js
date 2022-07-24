5bdf2529c2f65a5646859a0fa052388dc80f9390
Neo <himanshusharmadelhi2@kgpian.iitkgp.ac.in>
2022-07-24 17:36:03 +0530

    setting up to push to heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require('dotenv')
 
-mongoose.connect("mongodb://127.0.0.1:27017/SAC-Applications");
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGODB_URI || 
+  "mongodb://127.0.0.1:27017/SAC-Applications"
+);
+
+
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
