ae11b11ed2ef2559c4ec081cf631425175af112f
Neo <himanshusharmadelhi2@kgpian.iitkgp.ac.in>
2022-07-25 13:57:45 +0530

    added CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const chalk = require("chalk");
+const cors = require("cors");
 require("./db/mongoose");
 const ApplicationRouter = require("./routers/Application");
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 8000;
 
 
